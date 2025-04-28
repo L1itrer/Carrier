f57959f04f35d98fe7fb2b1877f444771561cccf
Litrer <106829230+L1itrer@users.noreply.github.com>
2025-04-28 10:57:09 +0200

    Add client skeleton

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,41 @@
+use std::net::{TcpStream};
+use std::process::exit;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let connection = TcpStream::connect("127.0.0.1:2137").unwrap_or_else(|err| {
+        eprintln!("Connect: {err}");
+        exit(1);
+    });
+    loop {
+        let mut command = String::new();
+        io::stdin().read_line(&mut command)
+            .expect("If read_line() fails god help you");
+        let mut words = command.split_whitespace();
+        let first_word=  words.next().unwrap_or_else( || {
+            "error"
+        });
+        if first_word.eq("exit") {
+            break
+        } else if first_word.eq("msg") {
+            let reciever =  words.next().unwrap_or_else(|| {
+                eprintln!("Usage: msg <login> <content of the message>");
+                "error"
+            });
+            if reciever.eq("error") {continue;}
+            let test_word = words.next().unwrap_or_else(|| {
+                eprintln!("Message must contain at least a single word!");
+                "error"
+            });
+            if test_word.eq("error") {continue;}
+            let msg = &command[(4+reciever.len()+1)..];
+            println!("{msg}");
+        } else if first_word.eq("login") {
+
+        }
+        else {
+            eprintln!("Unknown command!")
+        }
+
+    }
 }
